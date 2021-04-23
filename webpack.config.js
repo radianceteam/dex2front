a628e8a60fb41d65d8b562dfeab30db556fdf14c
senitskiy <po4ta4dima@yandex.ru>
2021-04-23 17:57:31 +0300

    mode: development

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
+  mode: 'development',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
