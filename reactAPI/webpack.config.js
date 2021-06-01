3b39b51e2f18886ee89b75ccd9300f08ce7324e9
MaxAkkerman <bagirokovanzor01@mail.ru>
2021-06-01 18:07:40 +0300

    reactAPI upload files

---
+++ reactAPI/webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path')
+const webpack = require('webpack')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+module.exports = {
+    devtool: 'inline-source-map',
+    mode: 'development',
+    devServer: {
+        contentBase: path.join(__dirname, './web'),
+        compress: true,
+        port: process.env.WEBPACK_DEV_SERVER_PORT || 3000,
+        disableHostCheck: true,
+        historyApiFallback: true,
+    },
+    entry: path.join(__dirname, './index.js'),
+    output: {
+        path: path.join(__dirname, './'),
+        publicPath: '/',
+        filename: '[name].bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                resolve: {
+                    extensions: [".js", ".jsx"]
+                },
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+        ]
+    },
+    plugins: [
+        new CopyWebpackPlugin({
+            patterns: [{ from: './node_modules/@tonclient/lib-web/tonclient.wasm' }],
+        }),
+        new webpack.LoaderOptionsPlugin({
+            minimize: false,
+            debug: true,
+        }),
+
+    ],
+
+}
