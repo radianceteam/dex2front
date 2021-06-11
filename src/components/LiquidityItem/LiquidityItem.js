baaeeba0100ea01e34b08866f5f639e3b245c917
Alexey Krivopusk <tigrovakyla11@gmail.com>
2021-06-11 15:06:19 +0300

    provide liquidity

--- src/components/LiquidityItem/LiquidityItem.js
+++ src/components/LiquidityItem/LiquidityItem.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import TON from '../../images/tokens/TON.svg';
-import wBTC from '../../images/tokens/wBTC.svg';
-import wETH from '../../images/tokens/wETH.svg';
-import wUSDT from '../../images/tokens/wUSDT.svg';
+import { iconGenerator } from '../../iconGenerator';
 import './LiquidityItem.scss';
 
-function LiquidityItem() {
+function LiquidityItem({walletAddress, symbol}) {
+  const symbols = symbol.split('/');
+  console.log(walletAddress);
   return (
     <div className="liquidity-item">
       <div>
-        <img src={TON} alt=""/>
-        <img src={wUSDT} alt=""/>
-        <span className="liquidity-item-text">TON/wUSDT</span>
+        <img src={iconGenerator(symbols[0])} alt={symbols[0]} />
+        <img src={iconGenerator(symbols[1])} alt={symbols[1]} />
+        <span className="liquidity-item-text">{symbols[0]}/{symbols[1]}</span>
       </div>
       <Link to={'/manage?fromSymbol=0:63e60c263fd73436caf57a8b783f078822c22bf761b6c0ad79cc9e218c5b6faa&toSymbol=0:327518a58690234e6baa5ae3724198cc8689c3d8b35f6433c07f12b06d796b0c'} className="liquidity-item-btn">Manage</Link>
     </div>
